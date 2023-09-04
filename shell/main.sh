5ac9ff1ffae0db7d2cc819d69b84ba22f03d51a7
hdoc <hector.ochoa.dev@gmail.com>
2023-09-04 17:56:45 -0500

    refactor(fzf): change config files extension to `.conf`

--- shell/main.sh
+++ shell/main.sh
@@ -61,10 +61,10 @@ else
 fi
 
 # FZF OPTIONS
-FZF_DEFAULT_COMMAND=$(tr '\n' ' ' <~/.dotfiles/fzf/command.txt)
+FZF_DEFAULT_COMMAND=$(tr '\n' ' ' <~/.dotfiles/fzf/command.conf)
 export FZF_DEFAULT_COMMAND
 
-FZF_DEFAULT_OPTS=$(tr '\n' ' ' <~/.dotfiles/fzf/options.txt)
+FZF_DEFAULT_OPTS=$(tr '\n' ' ' <~/.dotfiles/fzf/options.conf)
 export FZF_DEFAULT_OPTS
 
 # count git commits
