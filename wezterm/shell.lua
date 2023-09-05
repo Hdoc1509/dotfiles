59e9e1fce5a3bae4c35d8c6e677eada513df5e77
hdoc <blueguitarist1520@gmail.com>
2023-09-05 18:34:18 -0500

    fix(wezterm): use `/usr/bin/[shell_name]` for set shell if system is not `Windows`
    for some reason, it fails when check with `x86_64-unkon-linux-gnu`

--- wezterm/shell.lua
+++ wezterm/shell.lua
@@ -8,8 +8,8 @@ local linux_shell = '/usr/bin/' .. shell_name
 
 if user_os == 'x86_64-pc-windows-msvc' then
 	shell = { win_shell }
-elseif user_os == 'x86_64-unkown-linux-gnu' then
-	shell = { linux_shell }
+else
+  shell = { linux_shell }
 end
 
 table.insert(shell, '-l')
