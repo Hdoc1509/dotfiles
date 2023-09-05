b8dbbf87667338195eb304e94ce8491e7f73ff46
hdoc <blueguitarist1520@gmail.com>
2023-09-05 18:31:23 -0500

    style(wezterm): format `wezterm.lua`

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,38 +1,38 @@
 local wt = require('wezterm')
 
 return {
-	-- COLORS
-	color_scheme = 'carbonfox',
-	colors = require("colors"),
-	term = 'xterm-256color',
+  -- COLORS
+  color_scheme = 'carbonfox',
+  colors = require("colors"),
+  term = 'xterm-256color',
 
-	-- CURSOR
-	cursor_blink_ease_in = 'Constant',
-	cursor_blink_ease_out = 'Constant',
-	cursor_blink_rate = 500,
-	default_cursor_style = 'BlinkingBlock',
+  -- CURSOR
+  cursor_blink_ease_in = 'Constant',
+  cursor_blink_ease_out = 'Constant',
+  cursor_blink_rate = 500,
+  default_cursor_style = 'BlinkingBlock',
 
-	-- FONT
-	font = wt.font({
-		family = 'FiraCode NF',
-		harfbuzz_features = { 'liga=1' },
-	}),
-	font_size = 12.5,
+  -- FONT
+  font = wt.font({
+    family = 'FiraCode NF',
+    harfbuzz_features = { 'liga=1' },
+  }),
+  font_size = 12.5,
 
-	-- GUI
+  -- GUI
   hide_tab_bar_if_only_one_tab = true,
   show_new_tab_button_in_tab_bar = false,
-	window_close_confirmation = 'NeverPrompt',
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
+  window_close_confirmation = 'NeverPrompt',
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
 
-	-- KEY BINDINGS
-	keys = require("key_bindings"),
+  -- KEY BINDINGS
+  keys = require("key_bindings"),
 
-	-- SHELL
-	default_prog = require("shell"),
+  -- SHELL
+  default_prog = require("shell"),
 }
