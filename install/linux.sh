a5b845cbd954d4614caf177f49ddcbf94e053fe2
hdoc <blueguitarist1520@gmail.com>
2023-09-04 22:51:45 -0500

    fix(install): use `apt` for install `lsd` otherwise use `wget` and `sudo dpkg`. fixes #2

--- install/linux.sh
+++ install/linux.sh
@@ -7,7 +7,15 @@ echo "Symlinks installed!"
 echo "Installing apps..."
 sudo apt update
 sudo apt upgrade -y
-sudo apt install build-essential bat fd-find fzf gh jq lsd neovim nvm pnpm ripgrep shellcheck yarn
+sudo apt install build-essential bat fd-find fzf gh jq neovim nvm pnpm ripgrep shellcheck yarn
+
+echo "Installing lsd with apt\n"
+sudo apt install lsd ||
+  echo "\nTrying to install lsd with wget\n"
+  wget \
+    https://github.com/lsd-rs/lsd/releases/download/v1.0.0/lsd-musl_1.0.0_amd64.deb \
+    -O /tmp/lsd-install.deb &&
+    sudo dpkg -i /tmp/lsd-install.deb
 
 echo "Installing wezterm..."
 curl -LO https://github.com/wez/wezterm/releases/download/20230712-072601-f4abf8fd/WezTerm-20230712-072601-f4abf8fd-Ubuntu20.04.AppImage
