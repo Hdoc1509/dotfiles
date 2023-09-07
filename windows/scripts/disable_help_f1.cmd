7e8b91d6e44458c5c13b7abb2babfdb60731ff49
hdoc <hector.ochoa.dev@gmail.com>
2023-09-07 18:34:16 -0500

    feat(windows): add resource for script to disable help with key `f1`

--- windows/scripts/disable_help_f1.cmd
+++ windows/scripts/disable_help_f1.cmd
@@ -1,4 +1,4 @@
-REM Run with admin privilege
+REM Taken from: https://answers.microsoft.com/en-us/windows/forum/all/how-do-i-stop-f1-opening-a-browser-window-on-how/7f68f9b8-9d87-43a5-9a79-db87702254b5
 @echo off
 taskkill /f /im HelpPane.exe
 takeown /f %WinDir%\HelpPane.exe
