03af9a8d42be25107f32b35a035fcbff890b6972
hdoc <hector.ochoa.dev@gmail.com>
2024-02-20 22:27:47 -0500

    feat(wezterm): try background image

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,28 +1,40 @@
-local wt = require('wezterm')
+local wt = require("wezterm")
 
 return {
   -- COLORS
-  color_scheme = 'carbonfox',
+  color_scheme = "carbonfox",
   colors = require("colors"),
-  term = 'xterm-256color',
+  term = "xterm-256color",
 
   -- CURSOR
-  cursor_blink_ease_in = 'Constant',
-  cursor_blink_ease_out = 'Constant',
+  cursor_blink_ease_in = "Constant",
+  cursor_blink_ease_out = "Constant",
   cursor_blink_rate = 500,
-  default_cursor_style = 'BlinkingBlock',
+  default_cursor_style = "BlinkingBlock",
 
   -- FONT
   font = wt.font({
-    family = 'FiraCode NF',
-    harfbuzz_features = { 'liga=1' },
+    family = "FiraCode NF",
+    harfbuzz_features = { "liga=1" },
   }),
   font_size = 12,
 
   -- GUI
+  background = {
+    {
+      source = {
+        File = wt.config_dir .. "/backgrounds/bg-1.jpg",
+      },
+      vertical_align = "Middle",
+      horizontal_align = "Center",
+      hsb = {
+        brightness = 0.1,
+      },
+    },
+  },
   hide_tab_bar_if_only_one_tab = true,
   show_new_tab_button_in_tab_bar = false,
-  window_close_confirmation = 'NeverPrompt',
+  window_close_confirmation = "NeverPrompt",
   window_padding = {
     left = 0,
     right = 0,
