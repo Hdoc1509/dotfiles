e11d559374f0fae982abe909b4ebe02b10f18ae4
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-18 17:04:39 -0500

    refactor(fzf): remove checking for `msys` when exporting `FZF_DEFAULT_OPTS`
    not needed since `bat` is installed from its `.deb` package

--- fzf/fzf-options.sh
+++ fzf/fzf-options.sh
@@ -2,9 +2,4 @@ FZF_DEFAULT_COMMAND=$(tr '\n' ' ' <~/.dotfiles/fzf/command.conf)
 export FZF_DEFAULT_COMMAND
 
 FZF_DEFAULT_OPTS=$(tr '\n' ' ' <~/.dotfiles/fzf/options.conf)
-
-if ! is_msys_env; then
-  FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS//bat/batcat}"
-fi
-
 export FZF_DEFAULT_OPTS
